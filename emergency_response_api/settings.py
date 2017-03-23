4b6ae8c7fed52e9eabd1f1033f4e00b22e33345c
BrianHGrant <bhgrant8@gmail.com>
2017-03-23 12:29:59 -0700

    update secret key name

--- emergency_response_api/settings.py
+++ emergency_response_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = project_config.DJANGO_SECRET
+SECRET_KEY = project_config.DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
