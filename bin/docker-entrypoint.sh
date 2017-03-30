d1306ef0c0e6fc4ddcfd07a81b6e017badd5cf9f
BrianHGrant <bhgrant8@gmail.com>
2017-03-29 23:58:19 -0700

    remove workers and timeout

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -6,3 +6,4 @@ export PATH=$PATH:~/.local/bin
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 gunicorn emerresponseAPI.wsgi:application -b :8000
+# python manage.py runserver 0.0.0.0:8000
