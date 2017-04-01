6015ac8d126f0f57cb7ce0ccd89853bb5fea3860
BrianHGrant <bhgrant8@gmail.com>
2017-04-01 14:49:35 -0700

    remove migrate command as django is not responsible for db management

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -3,7 +3,6 @@
 export PATH=$PATH:~/.local/bin
 ./bin/getconfig.sh
 
-python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 gunicorn emerresponseAPI.wsgi:application -b :8000 -k 'gevent' -w 3
 # python manage.py runserver 0.0.0.0:8000
