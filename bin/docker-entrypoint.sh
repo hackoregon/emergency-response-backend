08804c07ea6eac73244358eff7f54dca16abbba7
BrianHGrant <bhgrant8@gmail.com>
2017-04-10 22:44:22 -0700

    remove extras from docker-entrypoint

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -2,8 +2,8 @@
 
 export PATH=$PATH:~/.local/bin
 ./bin/getconfig.sh
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+# python manage.py makemigrations --noinput
+# python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 gunicorn emerresponseAPI.wsgi:application -b :8000 -k 'gevent' -w 3
 # python manage.py runserver 0.0.0.0:8000
