a56d72607f9572f36f32c083232c856e082de9a9
Brian H. Grant <bhgrant8@gmail.com>
2017-04-17 13:35:25 -0700

    Update wsgi.py

--- emerresponseAPI/wsgi.py
+++ emerresponseAPI/wsgi.py
@@ -1,13 +1,13 @@
+
 """
 WSGI config for project project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
-import sys
-from gevent import monkey;
+
+from gevent import monkey; monkey.patch_all()
 from psycogreen.gevent import patch_psycopg
-monkey.patch_all()
 patch_psycopg()
 from whitenoise.django import DjangoWhiteNoise
 
