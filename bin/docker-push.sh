46a73b6ee3fbff2c2f1464464f279aa4b6881627
Mike Lonergan <MikeTheCanuck@users.noreply.github.com>
2018-06-04 21:26:38 -0700

    Revert "Improve AWS deployment script"

--- bin/docker-push.sh
+++ bin/docker-push.sh
@@ -2,18 +2,18 @@
 
 # Tag, Push and Deploy only if it's not a pull request
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  # Tag, Push and Deploy only if we're on the master branch
+  # Push only if we're testing the master branch
   if [ "$TRAVIS_BRANCH" == "master" ]; then
     export PATH=$PATH:$HOME/.local/bin
-    echo Getting the ECR login...
-    eval $(aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email)
-    echo Running docker push command...
+    echo Getting the ECR login... # Troubleshooting
+    eval $(aws ecr get-login --region $AWS_DEFAULT_REGION)
+    echo Running docker push command... # Troubleshooting
     docker push "$DOCKER_REPO"/"$DEPLOY_TARGET"/"$DOCKER_IMAGE":latest
-    echo Running ecs-deploy.sh script...
+    echo Running ecs-deploy.sh script... # Troubleshooting
     ./bin/ecs-deploy.sh  \
-     --service-name "$ECS_SERVICE_NAME" \
-     --cluster "$ECS_CLUSTER"   \
-     --image "$DOCKER_REPO"/"$DEPLOY_TARGET"/"$DOCKER_IMAGE":latest \
+     -n "$ECS_SERVICE_NAME" \
+     -c "$ECS_CLUSTER"   \
+     -i "$DOCKER_REPO"/"$DEPLOY_TARGET"/"$DOCKER_IMAGE":latest \
      --timeout 300
    else
      echo "Skipping deploy because branch is not master"
